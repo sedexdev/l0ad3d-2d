8b67fd541d8224a3666cd6030afa0378ab962df1
Andrew Macmillan <andrew.macmillan@ymail.com>
2023-11-27 16:35:38 +0000

    update doc string

--- src/classes/systems/EnemySystem.lua
+++ src/classes/systems/EnemySystem.lua
@@ -364,7 +364,7 @@ end
     Removes a grunt after its <isDead> attribute is set to true
 
     Params:
-        gruntID: bumber - ID of the grunt to remove
+        gruntID: number - ID of the grunt to remove
     Returns:
         nil
 ]]
